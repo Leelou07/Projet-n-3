d9158a3ce97157f42756f338e7839c6e2107ba13
Lou Jaouen <80525870+LouNuX@users.noreply.github.com>
2022-11-07 11:41:15 +0100

    Delete PFC_class_player.py

--- PFC_class_player.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self):
-        self.player = self._test()
-        print(self.player)
-   
-    def _test(self):        
-        test= input("Pierre, Feuille, Ciseaux ?")
-
