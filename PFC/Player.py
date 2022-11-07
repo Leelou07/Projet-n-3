30bbaa032796d3b44c52976245fdd73a2f04c9cf
Ouranoboros <pierre.moutiers@gmail.com>
2022-11-07 10:03:24 +0100

    V2

--- PFC/Player.py
+++ PFC/Player.py
@@ -1,11 +1,26 @@
 choices = ["Pierre", "Feuille", "Ciseaux"]
-verif = 0
-while verif == 0 :
-    def move():
-        print("Les coups possibles sont", choices)
-        return input("Quel est votre coup ?")
-    if move in choices :
-        print("D'accord")
-        verif = 1
+
+class user :
+    def start(self, name) :
+        self.name = name
+        print("Quel est votre Pseudo ?")
+        return(name)
+
+def choice ():    
+    print("Les coups possibles sont", choices)
+    print("Quel est votre coup ?")
+    return(choice)
+user_choice = choice
+
+x = 0
+def check ():
+    if choice in choices :
+        x = x+1
+        return("D'accord")
     else :
-        print("Votre coup n'existe pas !")
+        print("Votre coup n'est pas valide !")
+        choice
+
+while x == 0 :
+    choice
+    check
