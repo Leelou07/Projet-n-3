b8e63818d5a2c5a19f385cd25cba5c5137fff078
Ouranoboros <pierre.moutiers@gmail.com>
2022-11-09 13:07:13 +0100

    Ia répétition

--- PFC/Ia.py
+++ PFC/Ia.py
@@ -5,21 +5,7 @@ class Player :
         self.p_stat = 50
         self.f_stat = 50    
         self.c_stat = 0
-        
-        #Choix ia
-        self.ia_choice = self.ia_choice()
-       
-        #Choix joueur
-        self.choice = self.__choice()
 
-        #Proba IA
-        self.probability_actualisation_ia = self.probability_actualisation()
-
-        #Verif Prob
-        self.verif_proba = self.__verif_proba()
-
-        print("choix ia : " + str(self.ia_choice))
-           
     '''
     Partie Player
     '''
@@ -31,7 +17,7 @@ class Player :
     Partie Ia
     """
 
-    def ia_choice(self):
+    def __ia_choice(self):
         mylist = ["Pierre", "Feuille", "Ciseau"]
         ## Premier lancé
         if(self.p_stat == 50 and self.f_stat == 50 and self.c_stat == 0):
@@ -44,30 +30,34 @@ class Player :
         elif(self.f_stat > self.c_stat and self.f_stat > self.p_stat):
             return "Feuille"
         elif(self.p_stat == self.c_stat and self.p_stat > self.f_stat):
-            return str(random.choices(mylist, weights = [ 50, self.f_stat, 50], k=1))
+            return random.choices(mylist, weights = [ 50, self.f_stat, 50], k=1)
         elif(self.f_stat == self.p_stat and self.f_stat > self.c_stat):
-            return str(random.choices(mylist, weights = [ 50, 50, self.c_stat], k=1))
+            return random.choices(mylist, weights = [ 50, 50, self.c_stat], k=1)
         elif(self.c_stat == self.f_stat and self.c_stat > self.p_stat):
-            return str(random.choices(mylist, weights = [ self.p_stat, 50, 50], k=1))
+            return random.choices(mylist, weights = [ self.p_stat, 50, 50], k=1)
         elif(self.c_stat == self.f_stat and self.c_stat == self.p_stat):
-            return str(random.choices(mylist, weights = [ self.p_stat, self.f_stat, self.c_stat], k=1))
+            return random.choices(mylist, weights = [self.p_stat, self.f_stat, self.c_stat], k=1)
 
     def probability_actualisation(self):
-        self.ia_choice = self.ia_choice[0]
+        if type(self.ia_choice) != str:
+            self.ia_choice = self.ia_choice[0]
+        print(str(self.ia_choice) + " ici")
+        
+        print("ERREUR 1 : " + self.ia_choice)
         if self.ia_choice == "Pierre" : ## SI PIERRE VIENT DETRE JOUER
-            self.p_stat += 25
-            self.f_stat -= 25
-            self.c_stat -= 25
-
-        elif self.ia_choice == "Feuille" : ## SI FEUILLE VIENT DETRE JOUER
             self.p_stat -= 25
             self.f_stat += 25
-            self.c_stat -= 25
+            self.c_stat += 25
+
+        elif self.ia_choice == "Feuille" : ## SI FEUILLE VIENT DETRE JOUER
+            self.p_stat += 25
+            self.f_stat -= 25
+            self.c_stat += 25
 
         elif self.ia_choice == "Ciseau" : ## SI CISEAU VIENT DETRE JOUER
-            self.p_stat -= 25
-            self.f_stat -= 25
-            self.c_stat += 25   
+            self.p_stat += 25
+            self.f_stat += 25
+            self.c_stat -= 25   
 
     def __verif_proba(self):
         if(self.p_stat < 0):
@@ -76,7 +66,37 @@ class Player :
             self.f_stat = 0
         elif(self.c_stat < 0):
             self.c_stat = 0
+        if(self.p_stat >= 100):
+            self. p_stat = 100
+        elif(self.f_stat >= 100):
+            self.f_stat = 100
+        elif(self.c_stat >= 100):
+            self.c_stat = 100
+        
             
 
-Play = Player()
+    def __tour(self):
+        #Choix ia
+        self.ia_choice = self.__ia_choice()
+       
+        #Choix joueur
+        self.choice = self.__choice()
 
+        #Proba IA
+        self.probability_actualisation()
+
+        #Verif Prob
+        self.__verif_proba()
+
+        print("choix ia : " + self.ia_choice)
+        print(self.p_stat, self.f_stat, self.c_stat)
+
+    def repeat(self):
+        i = 0
+        while i < 40 :
+            self.tour = self.__tour()
+            i = i + 1
+
+
+Play = Player()
+Play.repeat()
